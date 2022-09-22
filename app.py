93f0ee0095961630ad5872b5e4c6ec187007ecd6
Alex780106 <show780106@gmail.com>
2022-09-22 14:09:10 +0800

    auto update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-import time
 
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('zYXfWsIEAgAiqGoU62iLIdiPDsIVsz5t1W0lr3urkwyemRmrfTZBCKauljVvoYqKFX71W08VEzNuIceqtoqWRBIiAefZAo8fKXJV9HgpxuCv+DXUIbVc5v8RwsMbRqb9J014bZrgo+e3TYZoPD3Y4AdB04t89/1O/w1cDnyilFU=')
@@ -10,7 +9,7 @@ yourID = 'U61a52960fc6fb48ae57da4f63bf06497'
 line_bot_api.push_message(yourID, 
                           TextSendMessage(text='安安您好！早餐吃了嗎？'))
 # 用迴圈推播訊息
-for i in [1,2,3,4,5]:
-    line_bot_api.push_message(yourID, 
-                              TextSendMessage(text='我們來倒數：'+str(i)))
-    time.sleep(1)
+# for i in [1,2,3,4,5]:
+#     line_bot_api.push_message(yourID, 
+#                               TextSendMessage(text='我們來倒數：'+str(i)))
+#     time.sleep(1)
