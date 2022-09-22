31213d7ae02c689161f6ac7a1a74f39e965b2d79
Alex780106 <show780106@gmail.com>
2022-09-22 12:52:51 +0800

    auto update

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from __future__ import unicode_literals
-from flask import Flask, request, abort, render_template
+# from __future__ import unicode_literals
+# from flask import Flask, request, abort, render_template
 from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
+# from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, ImageSendMessage, LocationSendMessage
-import requests
-import json
+# import requests
+# import json
 import configparser
-import os
-from urllib import parse
-app = Flask(__name__, static_url_path='/static')
-UPLOAD_FOLDER = 'static'
-ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+# import os
+# from urllib import parse
+# app = Flask(__name__, static_url_path='/static')
+# UPLOAD_FOLDER = 'static'
+# ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
 config = configparser.ConfigParser()
@@ -18,16 +18,16 @@ config.read('config.ini')
 
 def pushmsg(request):
     line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
-    handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+    # handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
     my_line_id = config.get('line-bot', 'my_line_id')
-    end_point = config.get('line-bot', 'end_point')
-    line_login_id = config.get('line-bot', 'line_login_id')
-    line_login_secret = config.get('line-bot', 'line_login_secret')
-    my_phone = config.get('line-bot', 'my_phone')
-    HEADER = {
-        'Content-type': 'application/json',
-        'Authorization': F'Bearer {config.get("line-bot", "channel_access_token")}'
-    }
+    # end_point = config.get('line-bot', 'end_point')
+    # line_login_id = config.get('line-bot', 'line_login_id')
+    # line_login_secret = config.get('line-bot', 'line_login_secret')
+    # my_phone = config.get('line-bot', 'my_phone')
+    # HEADER = {
+    #     'Content-type': 'application/json',
+    #     'Authorization': F'Bearer {config.get("line-bot", "channel_access_token")}'
+    # }
 
 
 # @app.route("/", methods=['POST', 'GET'])
