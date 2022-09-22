28102ddd1c0ba2ba4c0a3feaeda6f3cc442ce234
Alex780106 <show780106@gmail.com>
2022-09-22 12:30:26 +0800

    auto update

--- app.py
+++ app.py
@@ -72,25 +72,25 @@ HEADER = {
 
 
 def pushmsg(request):
-  try:
+#   try:
     msg = request.args.get('msg')
-    if msg == '1':
-      line_bot_api.push_message(my_line_id, TextSendMessage(text='hello'))
-    elif msg == '2':
-      line_bot_api.push_message(my_line_id, StickerSendMessage(package_id=1, sticker_id=2))
-    elif msg == '3':
-      imgurl = 'https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png'
-      line_bot_api.push_message(my_line_id, ImageSendMessage(original_content_url=imgurl, preview_image_url=imgurl))
-    elif msg == '4':
-      line_bot_api.push_message(my_line_id, LocationSendMessage(title='總統府',
+    # if msg == '1':
+    line_bot_api.push_message(my_line_id, TextSendMessage(text='hello'))
+    # elif msg == '2':
+    line_bot_api.push_message(my_line_id, StickerSendMessage(package_id=1, sticker_id=2))
+    # elif msg == '3':
+    imgurl = 'https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png'
+    line_bot_api.push_message(my_line_id, ImageSendMessage(original_content_url=imgurl, preview_image_url=imgurl))
+    # elif msg == '4':
+    line_bot_api.push_message(my_line_id, LocationSendMessage(title='總統府',
                                                 address='100台北市中正區重慶南路一段122號',
                                                 latitude='25.040319874750914',
                                                 longitude='121.51162883484746'))
-    else:
-      msg = 'ok'
+    # else:
+    msg = 'ok'
     return msg
-  except:
-    print('error')
+#   except:
+    # print('error')
 
 
 @handler.add(MessageEvent, message=TextMessage)
